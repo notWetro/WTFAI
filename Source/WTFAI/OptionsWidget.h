f649d15164dc4529ba916fcc4b63da36a1a5d820
HikmetG <hikmetgozaydin2002@gmail.com>
2025-07-12 12:09:57 +0200

    Options menu with Sound Slider

---
+++ Source/WTFAI/OptionsWidget.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Slider.h"
+#include "Components/Button.h"
+#include "Sound/SoundMix.h"
+#include "Sound/SoundClass.h"
+#include "OptionsWidget.generated.h"
+
+UCLASS()
+class WTFAI_API UOptionsWidget : public UUserWidget
+{
+    GENERATED_BODY()
+
+public:
+    virtual void NativeConstruct() override;
+
+protected:
+    UPROPERTY(meta = (BindWidget))
+    USlider* MasterVolumeSlider;
+
+    UPROPERTY(meta = (BindWidget))
+    UButton* BackButton;
+
+    UPROPERTY(EditAnywhere, Category = "Audio")
+    USoundMix* MasterSoundMix;
+
+    UPROPERTY(EditAnywhere, Category = "Audio")
+    USoundClass* MasterSoundClass;
+
+private:
+    UFUNCTION()
+    void OnMasterVolumeChanged(float NewValue);
+
+    UFUNCTION()
+    void OnBackClicked();
+};
---
+++ Source/WTFAI/VolumeSettings.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "VolumeSettings.generated.h"
+
+UCLASS()
+class WTFAI_API UVolumeSettings : public USaveGame
+{
+    GENERATED_BODY()
+public:
+    UPROPERTY(VisibleAnywhere, Category = "Settings")
+    float MasterVolume = 1.0f;
+};
